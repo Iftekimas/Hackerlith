76254ee190f6e1a6d76fdf40817ca458fca7a9de
Santiago <Santiagoforo@gmail.com>
2026-05-20 21:10:28 -0500

    agregar Controlador con logica base de movimiento

---
+++ src/co/edu/unbosque/controller/Controlador.java
@@ -0,0 +1,51 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.model.*;
+
+public class Controlador {
+
+    private Juego juego;
+
+    public Controlador() {
+        juego = new Juego(8, 8, 3, 64);
+        // filas - columnas - numPuertos - movimientosMax
+    }
+
+    public Juego getJuego() {
+        return juego;
+    }
+
+    public void moverJugador(String direccion) {
+        if (!juego.getEstado().equals(Juego.JUGANDO))
+            return;
+
+        int df = 0, dc = 0;
+        if (direccion.equals("W"))
+            df = -1;
+        else if (direccion.equals("S"))
+            df = 1;
+        else if (direccion.equals("A"))
+            dc = -1;
+        else if (direccion.equals("D"))
+            dc = 1;
+
+        int nuevaFila = juego.getJugador().getFila() + df;
+        int nuevaColumna = juego.getJugador().getColumna() + dc;
+
+        if (!juego.getTablero().estaEnRango(nuevaFila, nuevaColumna))
+            return;
+
+        String celda = juego.getTablero().getCelda(nuevaFila, nuevaColumna);
+
+        if (celda.equals("FIREWALL"))
+            return;
+
+        juego.getJugador().setFila(nuevaFila);
+        juego.getJugador().setColumna(nuevaColumna);
+        juego.getJugador().setMovimientosRestantes(juego.getJugador().getMovimientosRestantes() - 1);
+
+        juego.verificarVictoria();
+        juego.verificarDerrota();
+        juego.moverAmenazas();
+    }
+}
