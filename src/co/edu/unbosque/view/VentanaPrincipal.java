f1ade48352add23f130342bc9234d0c99ef3e07f
Santiago <Santiagoforo@gmail.com>
2026-05-20 22:38:15 -0500

    Añadir manejo de teclas para mover al jugador en VentanaPrincipal

--- src/co/edu/unbosque/view/VentanaPrincipal.java
+++ src/co/edu/unbosque/view/VentanaPrincipal.java
@@ -1,6 +1,8 @@
 package co.edu.unbosque.view;
 
 import javax.swing.JFrame;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import co.edu.unbosque.controller.Controlador;
 
@@ -17,6 +19,28 @@ public class VentanaPrincipal extends JFrame {
         panelJuego = new PanelJuego(controlador.getJuego());
         add(panelJuego);
 
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int code = e.getKeyCode();
+                String tecla = "";
+                if (code == KeyEvent.VK_W)
+                    tecla = "W";
+                else if (code == KeyEvent.VK_S)
+                    tecla = "S";
+                else if (code == KeyEvent.VK_A)
+                    tecla = "A";
+                else if (code == KeyEvent.VK_D)
+                    tecla = "D";
+
+                if (!tecla.isEmpty()) {
+                    controlador.moverJugador(tecla);
+                    panelJuego.actualizar();
+                }
+            }
+
+        });
+        setFocusable(true);
         setVisible(true);
     }
 
